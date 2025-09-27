b1f090017bd77caef3734980d87817be414dcb62
Andreas <git@fsck.rip>
2025-09-27 19:01:23 -0400

    add server
    Signed-off-by: Andreas <git@fsck.rip>

---
+++ Adoptly/src/server/server.ts
@@ -0,0 +1,31 @@
+import { getPetGemini } from "@/lib/petfinder";
+import jsonparser from "body-parser";
+import cors from "cors";
+import express, { type Express } from "express";
+
+export const createServer = (): Express => {
+  const app = express();
+
+  app.use(
+    cors({
+      origin: "*",
+      credentials: true,
+    })
+  );
+
+  app.disable("x-powered-by");
+  app.use(jsonparser.urlencoded({ extended: true }));
+  app.use(jsonparser.json());
+
+  app.get("/status", (_, res) => {
+    res.status(200).json({ ok: true });
+  });
+  app.get("/q", async (req, res) => {
+    const q = typeof req.query.q === "string" ? req.query.q : "";
+    const { text, pets } = await getPetGemini(q);
+
+    res.status(200).json({ ai: { response: text, query: q }, pets });
+  });
+
+  return app;
+};
