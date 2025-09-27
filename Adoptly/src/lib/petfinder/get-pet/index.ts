7696de1a919967343a078112cbdf893df7635e59
Andreas <git@fsck.rip>
2025-09-27 19:02:22 -0400

    move pet files to their own locations
    Signed-off-by: Andreas <git@fsck.rip>

--- Adoptly/src/lib/petfinder/get-pet/index.ts
+++ Adoptly/src/lib/petfinder/get-pet/index.ts
@@ -1,9 +1,7 @@
-import { ai, GEMINI_DEFAULT_MODEL } from "@/lib/gemini";
 import { Type, type FunctionDeclaration } from "@google/genai/web";
-import { getAnimalTypes, getAnimalTypesDeclaration } from "../animal-types";
 import { pf } from "..";
 
-const getPetFunctionDeclaration = {
+export const getPetFunctionDeclaration = {
   name: "getPet",
   description: "Get a pet based on a user's description.",
   parameters: {
@@ -33,77 +31,21 @@ const getPetFunctionDeclaration = {
 type GetPetFunctionDeclaration =
   typeof getPetFunctionDeclaration.parameters.properties;
 
-async function getPet(args: GetPetFunctionDeclaration) {
+export async function getPet(args: GetPetFunctionDeclaration) {
   await pf.authenticate();
-  const response = await pf.animal.search({
-    type: args?.animal_types,
-    breed: args?.breed,
-    size: args?.size,
-    limit: args?.limit,
-  });
+  let response;
+  try {
+    response = await pf.animal.search({
+      type: args?.animal_types,
+      breed: args?.breed,
+      size: args?.size,
+      limit: args?.limit,
+      location: "Baltimore, Maryland",
+    });
+  } catch (err) {
+    console.error(err);
+    throw new Error("Something went wrong.", { cause: err });
+  }
 
-  // Extract only the serializable data from the response
   return response.data.animals;
 }
-
-const functions = {
-  getPet,
-  getAnimalTypes,
-};
-
-export async function getPetGemini(prompt: string) {
-  const chat = ai.chats.create({
-    model: GEMINI_DEFAULT_MODEL,
-    config: {
-      tools: [
-        {
-          functionDeclarations: [
-            getPetFunctionDeclaration,
-            getAnimalTypesDeclaration,
-          ],
-        },
-      ],
-    },
-  });
-  const wholePrompt = prompt;
-
-  let response = await chat.sendMessage({
-    message: wholePrompt,
-  });
-
-  
-  if (response.functionCalls && response.functionCalls.length > 0) {
-    for (const call of response.functionCalls) {
-      const fnName = call.name as string;
-      if (!functions[fnName]) throw new Error(`Function not found`);
-      const fnResponse = await functions[fnName](call.args);
-      
-      response = await chat.sendMessage({
-        message: {
-          functionResponse: {
-            name: fnName,
-            response: { result: { ...fnResponse } },
-          },
-        },
-      });
-    }
-  }
-  console.log(response.candidates[0].content?.parts[0].functionCall);
-  
-  const parts = response.candidates?.[0].content?.parts;
-  let full = "";
-  if (parts) {
-    for (const part of parts) {
-      if (part.text) {
-        full += part.text;
-      }
-    }
-  }
-  console.log(full);
-
-  return full;
-}
-
-getPetGemini(
-  "gimme some animals that is cat, ideally tabby cats. show me like 20"
-);
--- Adoptly/src/lib/petfinder/index.ts
+++ Adoptly/src/lib/petfinder/index.ts
@@ -1,6 +1,66 @@
-import petfinder from "@petfinder/petfinder-js";
+import { Client } from "@petfinder/petfinder-js";
+import { ai, GEMINI_DEFAULT_MODEL } from "../gemini";
+import { getAnimalTypes, getAnimalTypesDeclaration } from "./animal-types";
+import { getPet, getPetFunctionDeclaration } from "./get-pet";
 
 export const pf = new petfinder.Client({
   apiKey: import.meta.env.VITE_PF_API_KEY,
   secret: import.meta.env.VITE_PF_SECRET,
 });
+const functions = {
+  getPet,
+  getAnimalTypes,
+};
+
+export async function getPetGemini(prompt: string) {
+  const chat = ai.chats.create({
+    model: GEMINI_DEFAULT_MODEL,
+    config: {
+      tools: [
+        {
+          functionDeclarations: [
+            getPetFunctionDeclaration,
+            getAnimalTypesDeclaration,
+          ],
+        },
+      ],
+    },
+  });
+  const wholePrompt = prompt;
+
+  let response = await chat.sendMessage({
+    message: wholePrompt,
+  });
+
+  const pets = [];
+  if (response.functionCalls && response.functionCalls.length > 0) {
+    for (const call of response.functionCalls) {
+      const fnName = call.name as keyof typeof functions;
+      if (!functions[fnName]) throw new Error(`Function not found`);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const fnResponse = await functions[fnName](call.args as unknown as any);
+      pets.push(...fnResponse);
+
+      response = await chat.sendMessage({
+        message: {
+          functionResponse: {
+            name: fnName,
+            response: { result: fnResponse },
+          },
+        },
+      });
+    }
+  }
+
+  const parts = response.candidates?.[0]?.content?.parts;
+  let full = "";
+  if (parts) {
+    for (const part of parts) {
+      if (part.text) {
+        full += part.text;
+      }
+    }
+  }
+
+  return { text: full, pets: pets ?? [] };
+}
